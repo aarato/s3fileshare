f39a626fb82934f6d270e34113d7f791b2ddd57c
andras@arato.biz <andras@arato.biz>
2023-03-08 15:23:43 -0600

    re

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,20 @@ data "template_file" "aws_config" {
     userPoolId     = aws_cognito_user_pool.pool.id
     clientId       = aws_cognito_user_pool_client.client.id
     identityPoolId = aws_cognito_identity_pool.id_pool.id
-    websocket_api  = aws_apigatewayv2_stage.stage_prod.invoke_url
+
   }
 }
+
+# data "template_file" "aws_config" {
+#   template = file("./templates/awsconfig.json")
+
+#   vars = {
+#     region         = var.region
+#     bucket         = aws_s3_bucket.account.id
+#     userPoolId     = aws_cognito_user_pool.pool.id
+#     clientId       = aws_cognito_user_pool_client.client.id
+#     identityPoolId = aws_cognito_identity_pool.id_pool.id
+#     websocket_api  = aws_apigatewayv2_stage.stage_prod.invoke_url
+#   }
+# }
 
