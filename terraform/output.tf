9fec3e4788df63e589c4f6168d2ded25368e01fb
andras@arato.biz <andras@arato.biz>
2023-03-08 11:41:28 -0600

    adding user pool id to tf output

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,6 +7,10 @@ output "websocket_api" {
   value = aws_apigatewayv2_stage.stage_prod.invoke_url
 }
 
+output "aws_cognito_user_pool_id" {
+  value = aws_cognito_user_pool.pool.id
+}
+
 # output "result_entry" {
 #   value = jsondecode(data.aws_lambda_invocation.create_admin.result)
 # }
