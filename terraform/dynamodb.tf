07dab652d6ab0b9c12cef230983682ff5d1bb0dd
andras@arato.biz <andras@arato.biz>
2023-03-08 15:48:33 -0600

    rrrrrrr

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -4,7 +4,7 @@
 
 module "dynamodb_table" {
   source   = "terraform-aws-modules/dynamodb-table/aws"
-
+  version = "~> 3.1.2"
   name     = "${var.name}_clipboard_connection_table"
   hash_key = "connectionId"
 
