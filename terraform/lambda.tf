4b0496056b5914c556efae2121516bc4a3538362
aarato <andras@arato.biz>
2023-04-20 17:41:46 -0500

    rrr

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@
 
 data "aws_lambda_invocation" "create_admin" {
   # function_name = module.lambda_create_cognito_user.lambda_function_name
-  function_name = aws_lambda_function.function.function_name
+  function_name = aws_lambda_function.create_cognito_user.function_name
 
   input = <<JSON
 {
@@ -111,39 +111,36 @@ module "lambda_clipboard_sendmessage" {
 
 
 locals {
-  function_name               = "${lower(var.name)}-${lower(var.app_environment)}-create_cognito_user"
-  function_handler            = "index.lambda_handler"
-  function_runtime            = "python3.9"
-  function_source_dir         = "${path.module}/aws_lambda_functions/create_cognito_user"
-  function_zip_file           = "${path.module}/aws_lambda_functions/${local.function_name}.zip"
-  function_timeout_in_seconds = 5
+  create_cognito_user_name               = "${lower(var.name)}-${lower(var.app_environment)}-create_cognito_user"
+  create_cognito_user_source_dir         = "${path.module}/aws_lambda_functions/create_cognito_user"
+  create_cognito_user_zip_file           = "${path.module}/aws_lambda_functions/${local.function_name}.zip"
 }
 
 
-resource "aws_lambda_function" "function" {
-  function_name = local.function_name
-  description = local.function_name
+resource "aws_lambda_function" "create_cognito_user" {
+  function_name = local.create_cognito_user_name
+  description = local.create_cognito_user_name
   handler       = "index.lambda_handler"
   runtime       = "python3.9"
   timeout       = 5
 
-  filename         = local.function_zip_file
-  source_code_hash = data.archive_file.function_zip.output_base64sha256
-  role = aws_iam_role.function_role.arn
+  filename         = local.create_cognito_user_zip_file
+  source_code_hash = data.archive_file.create_cognito_user.output_base64sha256
+  role = aws_iam_role.create_cognito_user.arn
   depends_on = [
     aws_cognito_user_pool.pool,
     aws_cloudwatch_log_group.lambda_create_cognito_user
   ]
 }
 
-data "archive_file" "function_zip" {
+data "archive_file" "create_cognito_user" {
   type        = "zip"
-  source_dir = local.function_source_dir
-  output_path = local.function_zip_file
+  source_dir = local.create_cognito_user_source_dir
+  output_path = local.create_cognito_user_zip_file
 }
 
-resource "aws_iam_role" "function_role" {
-  name = "${local.function_name}-role"
+resource "aws_iam_role" "create_cognito_user" {
+  name = "${local.create_cognito_user_name}-role"
 
   assume_role_policy = jsonencode({
     Statement = [
