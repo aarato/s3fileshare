a4548756b61a5a2b3a023565c73c88052288ea48
aarato <andras@arato.biz>
2023-04-20 20:36:26 -0500

    ff

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -41,7 +41,7 @@ resource "aws_apigatewayv2_integration" "clipboard_connect" {
   content_handling_strategy = "CONVERT_TO_TEXT"
   description               = "clipboard_connect"
   integration_method        = "POST"
-  integration_uri           = module.lambda_clipboard_onconnect.lambda_function_invoke_arn
+  integration_uri           = aws_lambda_function.clipboard_onconnect.invoke_arn
   passthrough_behavior      = "WHEN_NO_MATCH"
 }
 resource "aws_apigatewayv2_integration" "clipboard_disconnect" {
