546ea81434b662cff4ee9ad59dafc924e422b6cb
aarato <ucipass@gmail.com>
2023-04-21 20:35:00 -0500

    V3 delete

--- dist/js/chunk-8daa1a00.6925d511.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-8daa1a00"],{6:function(n,w){}}]);
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  productionSourceMap: false,
-  configureWebpack: {
-    devtool: 'source-map',
-    },  
-  outputDir: "dist", // Build directory 
-  publicPath: '' 
-} 
